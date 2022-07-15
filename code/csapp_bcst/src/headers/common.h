7a810183b4de55036eb2f76740a2dff950775fab
holden <holden@DESKTOP-0T5K9IJ.localdomain>
2022-07-15 22:36:01 +0800

    finish code refactory of video 0x8 in bcst_csapp

---
+++ code/csapp_bcst/src/headers/common.h
@@ -0,0 +1,38 @@
+// include guard to prevent double declaration of any identifiers
+// such as types, enums and static variables
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#include <stdint.h>
+
+#define DEBUG_INSTRUCTIONCYCLE  0X1
+#define DEBUG_REGISTERS         0X2
+#define DEBUG_PRINTSTACK        0X4
+#define DEBUG_PRINTCACHESET     0X8
+#define DEBUG_CACHEDETAILS      0X10
+#define DEBUG_MMU               0X20
+#define DEBUG_LINKER            0X40
+#define DEBUG_LOADER            0X80
+#define DEBUG_PARSEINST         0X100
+
+// to control the verbose degree of debug_printf
+#define DEBUG_VERBOSE_SET       0X41
+
+// do page walk
+#define DEBUG_ENABLE_PAGE_WALK  0
+
+// use sram cache for memory access
+#define DEBUG_ENABLE_SRAM_CACHE 0
+
+// printf wrapper
+uint64_t debug_printf(uint64_t open_set, const char *format, ...);
+
+// type converter
+// uint32 to its equivalent float with rounding
+uint32_t uint2float(uint32_t u);
+
+// convert string dec or hex to the integer bitmap
+uint64_t string2uint(const char *str);
+uint64_t string2uint_range(const char *str, int start, int end);
+
+#endif
---
+++ code/csapp_bcst/src/headers/memory.h
@@ -0,0 +1,32 @@
+// include guards to prevent double declaration of any identifiers
+// such as types, enums and static variables
+#ifndef __MEMORY_H
+#define __MEMORY_H
+
+#include <stdint.h>
+#include <headers/cpu.h>
+
+/*=======================================*/
+/*     physical memory on dram chips     */
+/*=======================================*/
+
+// physical memory space is decided by the physical address
+// in this simulator, there are 4 + 6 + 6 = 16 bit physical address
+// then the physical space is (1 << 16) = 65536
+// total 16 physical memory
+#define PHYSICAL_MEMORY_SPACE 65536
+#define MAX_INDEX_PHYSICAL_PAGE 15
+
+// physical memory
+// 16 physical memory pages
+uint8_t pm[PHYSICAL_MEMORY_SPACE];
+
+/*=======================================*/
+/*               memory R/W              */
+/*=======================================*/
+
+// used by instructions: read or write uint64_t to DRAM
+uint64_t read64bits_dram(uint64_t paddr, core_t *cr);
+void write64bits_dram(uint64_t paddr, uint64_t data, core_t *cr);
+
+#endif
